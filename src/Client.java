32826289eeb8d18c1fa2e83db9927c6cab88fc24
Denis Shumov <75227278+shumovdenis@users.noreply.github.com>
2021-01-13 00:56:54 +0300

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,30 @@
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8080;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(clientSocket.getInputStream()))){
+            out.println("GET / HTTP/1.1\n" +
+                    "Host: localhost \n\n\n");
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (IIOException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
